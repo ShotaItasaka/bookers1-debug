80bab6adfce79bb445907bf3f1a9f273c5893698
ShotaItasaka <shotachemistryt@gmail.com>
2023-06-02 16:28:38 +0000

    最終修正版

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,7 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
